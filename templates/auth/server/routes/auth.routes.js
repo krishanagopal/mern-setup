44159c888f1d5365cbbdd526ec0a003e36ec1021
krishanagopal <gopalkumardevna@gmail.com>
2026-01-04 01:55:28 +0530

    setting of loginroute and testing login api

--- templates/auth/server/routes/auth.routes.js
+++ templates/auth/server/routes/auth.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { registerUser } from "../controllers/auth.controller.js";
+import { registerUser,loginUser } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 // Signup route
 router.post("/register", registerUser);
+
+// Login route
+router.post("/login", loginUser);
+
 
 export default router;
