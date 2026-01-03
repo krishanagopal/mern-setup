f4c6804c15ed2e704e7c936d95158986ce4c40f4
krishanagopal <gopalkumardevna@gmail.com>
2026-01-04 01:53:57 +0530

    login api created

--- templates/auth/server/controllers/auth.controller.js
+++ templates/auth/server/controllers/auth.controller.js
@@ -1,6 +1,11 @@
 import User from "../modals/user.model.js";
 import { hashPassword } from "../utils/hash.js";
 
+
+
+//logic for user signup
+
+
 export const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -46,3 +51,53 @@ export const registerUser = async (req, res) => {
     });
   }
 };
+
+
+//logic for login api
+import { comparePassword } from "../utils/hash.js";
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // 1. Validation
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password are required",
+      });
+    }
+
+    // 2. Find user
+   const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    // 3. Compare password
+    const isMatch = await comparePassword(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    // 4. Success response
+    res.status(200).json({
+      message: "Login successful",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
+
